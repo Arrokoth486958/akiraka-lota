f230035845da2bdbf57fcf8533c36e14eb1b337f
Arrokoth <arrokoth233@gmail.com>
2023-11-05 11:36:04 +0100

    我们还不确定.jpg

--- ui/src/wgpu/texture.rs
+++ ui/src/wgpu/texture.rs
@@ -1,27 +1,25 @@
+use std::vec;
+
 use wgpu::{
     util::{BufferInitDescriptor, DeviceExt},
     BindGroup, BindGroupDescriptor, BindGroupEntry, BindGroupLayout, BindingResource, BufferUsages,
     CommandEncoderDescriptor, Extent3d, ImageCopyTexture, ImageDataLayout, Origin3d, RenderPass,
-    SamplerDescriptor, TextureAspect, TextureFormat, TextureUsages, TextureViewDescriptor,
+    SamplerDescriptor, TextureAspect, TextureFormat, TextureUsages, TextureViewDescriptor, Buffer, Queue,
 };
 
 use super::WGPUInstance;
 
-static mut BIND_GROUPS: Vec<BindGroup> = Vec::new();
-
 pub struct Texture {
-    // bind_group_layout: Layout,
-    // bind_group_ptr: *mut u8,
-    // bind_group: &'static BindGroup,
-    pub bind_group_position: usize,
     pub size: Extent3d,
     pub texture: wgpu::Texture,
+    // 很生草的玩意
+    pub data: Vec<u8>,
+    pub buffer: Buffer,
 }
 
 impl Texture {
     pub fn from_bytes(
         bytes: &[u8],
-        texture_bind_group_layout: &BindGroupLayout,
         instance: &WGPUInstance,
     ) -> Texture {
         let image = image::load_from_memory(bytes).unwrap();
@@ -68,114 +66,24 @@ impl Texture {
             usage: BufferUsages::COPY_SRC,
         });
 
-        let mut encoder = instance
-            .device
-            .create_command_encoder(&CommandEncoderDescriptor {
-                label: Some("texture_buffer_copy_encoder"),
-            });
-
-        encoder.copy_buffer_to_texture(
-            wgpu::ImageCopyBuffer {
-                buffer: &buffer,
-                layout: ImageDataLayout {
-                    offset: 0,
-                    bytes_per_row: Some(4 * dimensions.0),
-                    rows_per_image: Some(dimensions.1),
-                },
-            },
-            ImageCopyTexture {
-                texture: &texture,
-                mip_level: 0,
-                aspect: TextureAspect::All,
-                origin: Origin3d::ZERO,
-            },
-            texture_size,
-        );
-
-        instance.queue.submit(Some(encoder.finish()));
-
-        let texture_view = texture.create_view(&TextureViewDescriptor::default());
-        let sampler = instance.device.create_sampler(&SamplerDescriptor {
-            address_mode_u: wgpu::AddressMode::ClampToEdge,
-            address_mode_v: wgpu::AddressMode::ClampToEdge,
-            address_mode_w: wgpu::AddressMode::ClampToEdge,
-            mag_filter: wgpu::FilterMode::Linear,
-            min_filter: wgpu::FilterMode::Nearest,
-            mipmap_filter: wgpu::FilterMode::Nearest,
-            ..Default::default()
-        });
-
-        // let texture_bind_group_layout =
-        //     instance.device.create_bind_group_layout(&BindGroupLayoutDescriptor {
-        //         entries: &[
-        //             BindGroupLayoutEntry {
-        //                 binding: 0,
-        //                 visibility: ShaderStages::FRAGMENT,
-        //                 ty: BindingType::Texture {
-        //                     multisampled: false,
-        //                     view_dimension: TextureViewDimension::D2,
-        //                     sample_type: TextureSampleType::Float { filterable: true },
-        //                 },
-        //                 count: None,
-        //             },
-        //             BindGroupLayoutEntry {
-        //                 binding: 1,
-        //                 visibility: ShaderStages::FRAGMENT,
-        //                 ty: BindingType::Sampler(SamplerBindingType::Filtering),
-        //                 count: None,
-        //             },
-        //         ],
-        //         label: Some("texture_bind_group_layout"),
-        //     });
-
-        let bind_group = instance.device.create_bind_group(&BindGroupDescriptor {
-            layout: &texture_bind_group_layout,
-            entries: &[
-                BindGroupEntry {
-                    binding: 0,
-                    resource: BindingResource::TextureView(&texture_view),
-                },
-                BindGroupEntry {
-                    binding: 1,
-                    resource: BindingResource::Sampler(&sampler),
-                },
-            ],
-            label: Some("bind_group"),
-        });
-
-        let bind_group_position = unsafe {
-            let mut x: usize = 0;
-            for i in 0..BIND_GROUPS.len() {
-                if BIND_GROUPS.get(i).is_none() {
-                    x = i
-                }
-            }
-            BIND_GROUPS.insert(x, bind_group);
-            x
-        };
+        let data = Vec::from(bytes);
 
         Texture {
-            bind_group_position,
             size: texture_size,
             texture,
+            data,
+            buffer,
         }
     }
 
-    pub fn bind(&mut self, render_pass: &mut RenderPass) {
-        render_pass.set_bind_group(
-            0,
-            unsafe { BIND_GROUPS.get(self.bind_group_position.clone()).unwrap() },
-            &[],
-        );
+    pub fn bind(&mut self, queue: &mut Queue) {
+        // let boxed_slice = self.data.into_boxed_slice();
+        // queue.write_buffer(&self.buffer, 0, self.data.collec)
     }
 
     // 一定要调用！！！
     // 不然内存泄漏把你搞哭
     pub fn destroy(&mut self) {
         self.texture.destroy();
-        unsafe {
-            // std::alloc::dealloc(self.bind_group_ptr, self.bind_group_layout);
-            BIND_GROUPS.remove(self.bind_group_position);
-        }
     }
 }
